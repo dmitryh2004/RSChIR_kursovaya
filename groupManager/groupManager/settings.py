0f9e21b5ae0d3305651026f55f48179101632781
dmitry_h <113709033+dmitryh2004@users.noreply.github.com>
2024-12-02 17:56:09 +0300

    1

--- groupManager/groupManager/settings.py
+++ groupManager/groupManager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a5*gw!$z(msv!r)4&hks@l%f807g-2q!tiai_6g05aflqbpog0'
+SECRET_KEY = 'django-insecure-a5*gw!$z(msv!r)4&hks@l%f807g-2q!tiai_6g05aflqbpog1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
